4fe033bbe216ad04dd137da2ac4562f5036a9b25
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-15 11:54:40 -0600

    completed program

--- app/src/main/java/edu/fvtc/sharedpreferencesdemo/EditDarkMode.java
+++ app/src/main/java/edu/fvtc/sharedpreferencesdemo/EditDarkMode.java
@@ -3,18 +3,59 @@ package edu.fvtc.sharedpreferencesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class EditDarkMode extends AppCompatActivity {
-
+    public static final String TAG = "EditDarkMode";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_dark_mode);
 
         initButton();
+        initCheckBox();
+        getSettings();
+
+    }
+
+    private void getSettings() {
+        // Read the shared preferences
+        SharedPreferences preferences = getApplication().getSharedPreferences("myprefs", MODE_PRIVATE);
+        Boolean isDarkMode = preferences.getBoolean("darkmode", false);
+        Log.d(TAG, "onResume: " + isDarkMode);
+
+        CheckBox chkDarkMode = findViewById(R.id.chkDarkMode);
+        chkDarkMode.setChecked(isDarkMode);
+    }
+
+    private void initCheckBox() {
+        CheckBox chkDarkMode = findViewById(R.id.chkDarkMode);
+
+        chkDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Save the value of the checkbox to SharedPreferences
+
+                SharedPreferences preferences = getApplication().getSharedPreferences("myprefs", MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.putBoolean("darkmode", isChecked);
+                editor.putInt("age", 45);
+                editor.putString("name", String.valueOf(R.string.app_name));
+
+                editor.commit();
+                Log.d(TAG, "onCheckedChanged: " + isChecked);
+
+
+            }
+        });
     }
 
     private void initButton() {
@@ -24,9 +65,9 @@ public class EditDarkMode extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Go to the other screen
-                startActivity(new Intent(v.getContext(), MainActivity.class));
-
+                startActivity(new Intent(v.getContext(),MainActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/edu/fvtc/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/edu/fvtc/sharedpreferencesdemo/MainActivity.java
@@ -3,11 +3,15 @@ package edu.fvtc.sharedpreferencesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         initButton();
     }
 
+    @Override
+    public void onResume()
+    {
+        // Read the shared preferences
+        SharedPreferences preferences = getApplication().getSharedPreferences("myprefs", MODE_PRIVATE);
+        Boolean isDarkMode = preferences.getBoolean("darkmode", false);
+        Log.d(TAG, "onResume: " + isDarkMode);
+
+        CheckBox chkDarkMode = findViewById(R.id.chkDarkMode);
+        chkDarkMode.setChecked(isDarkMode);
+        super.onResume();
+    }
     private void initButton() {
         Button btnNavigate = findViewById(R.id.btnNavigate);
 
@@ -24,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Go to the other screen
-                startActivity(new Intent(v.getContext(), EditDarkMode.class));
-
+                startActivity(new Intent(v.getContext(),EditDarkMode.class));
             }
         });
+
     }
 }
